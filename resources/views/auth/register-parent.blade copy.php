e7010e86a144f5bd14d790e102a01652cdcaa28f
riohifi <riohifi01@gmail.com>
2023-02-24 22:08:35 +0530

    fixed design

--- resources/views/auth/register-parent.blade copy.php
+++ resources/views/auth/register-parent.blade copy.php
@@ -4,20 +4,13 @@
 <div class="container ">
     <div class="row justify-content-center ">
         <div class="col-md-6 d-flex align-items-center justify-content-center">
-            <img class="img-fluid" src="/images/member.jpg" />
+            <div class="">
+                <h2 class=""><b>Register</b></h2>
+                
+                <h2 class=""><b>Now</b></h2>
+            </div>
         </div>
-        <div class="col-md-6">
-            {{-- <div class="card">
-                <div class="row  md-3 justify-content-center">
-                    <div class="col-md-8">
-                        <label for="firstName">First name</label>
-                            <input type="text" class="form-control" name="name" id="firstName" placeholder="" value="" required>
-                            <div class="invalid-feedback">
-                              Valid first name is required.
-                            </div>
-                    </div>
-                </div>
-            </div> --}}
+        <div class="col-md-4">
             <div class="card">
                 @if ($errors->any())
             @foreach ($errors->all() as $error)
@@ -29,28 +22,17 @@
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
-               
+                <div class="row md-3 justify-content-center">
+                    <div class="col-md-6">
+                        
+                    </div>
+                </div>
                 <div id="parent" class="card-body">
                     
-                    <form id="register"  class="needs-validation" novalidate method="POST" action="{{route('register')}}">
+                    <form id="register"  class="needs-validation" novalidate method="POST" action="{{route('register.parent')}}">
                         @csrf
+                        
                         <div class="row">
-                            <div class="col-md-12">
-                                <input type="text"  class="form-control text-center" name="code" id="code" placeholder="Membership Code" value="" required>
-                                <div class="invalid-feedback">
-                                  Valid first name is required.
-                                </div>
-                            </div> 
-                            <div class="col-md-12 mt-3">
-                                <button type="button" class="btn btn-outline-primary btn-block buttonload"  id="load">
-                                    <span id="chkbtn">Check</span>
-                                    <span id="spin"><i class="fa fa-spinner fa-spin"></i> Incorrect</span>
-                                    <span id="tic"><i class="fa fa-check text-white" aria-hidden="true"></i></span> 
-                                </button>
-                            </div>                               
-                        </div>
-                        <div id="main">
-                        <div class="row mt-3">
                           <div class="col-md-6 mb-3">
                             <label for="firstName">First name</label>
                             <input type="text" class="form-control" name="name" id="firstName" placeholder="" value="" required>
@@ -103,24 +85,22 @@
                               Zip code required.
                             </div>
                           </div>
-                          
+                          <div class="mb-3">
+                            <label for="password">Password <span class="text-muted">*</span></label>
+                            <input type="text" class="form-control" name="password" id="password" placeholder="Password">
+                          </div>
+                          <div class="mb-3">
+                            <label for="password_confirmation">Confirm Password <span class="text-muted">*</span></label>
+                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Password">
+                          </div>
                         </div>
                        
-                       <div class="row">
-                        <div class="col-md-6 mb-3">
-                              <label for="password">Password <span class="text-muted">*</span></label>
-                              <input type="text" class="form-control" name="password" id="password" placeholder="Password">
-                            </div>
-                            <div class="col-md-6 mb-3">
-                              <label for="password_confirmation">Confirm Password <span class="text-muted">*</span></label>
-                              <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Password">
-                            </div>
-                       </div>
+                       
                        
             
                         <button class="btn btn-primary btn-lg btn-block" type="submit">Register</button>
                         
-                    </div>
+                        
                       </form>
                 </div>
                 
@@ -132,9 +112,7 @@
 @push('script')
 <script language = "javascript">
     $(document).ready(function() {
-        $('#main').hide();
-        $('#spin').hide();
-        $('#tic').hide();
+        $('#child').hide();
         $('#check_parent').click(function(){
             if ($(this).is(':checked')) {
                 $('#parent').hide();
@@ -144,29 +122,11 @@
                 $('#parent').show();
             }
         })
-        $('#code').keyup(function(){
-            $('#spin').show();
-        });
-        $('#code').change(function(){
-            var data=$(this).val();
-            $('#spin').show();
-            $('#chkbtn').hide();
-            $.get("{{route('user.check_code')}}"+"?code="+data,function(result){
-                console.log(result);
-            if(result==1){
-                $('#main').show();
-                $('#spin').hide();
-                $('#tic').show();
-                $('#load').addClass('bg-success');
-                $('#load').removeClass('bg-danger');
-            }else{
-                $('#spin').show();
-                $('#tic').hide();
-                $('#load').addClass('bg-danger');                
-            }
-            });
-        });
-        
+        // $("#parent").click(function(){
+        //    $('#login').removeAttr('hidden');
+        //    $('#register').attr('hidden',true);
+        //    $('#login_btn').attr('hidden',true);
+        // });
     });
 </script>
 @endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    {{-- <link href="/css/styles2.css" rel="stylesheet" /> --}}
+    <link href="/css/admin/styles.css" rel="stylesheet" /> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"/>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
@@ -16,80 +17,13 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.2/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.bootstrap5.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.13.2/sl-1.6.0/datatables.min.css"/>
-    <script>
-        body{margin-top:20px;
-background-color:#f2f6fc;
-color:#69707a;
-}
-.img-account-profile {
-    height: 10rem;
-}
-.rounded-circle {
-    border-radius: 50% !important;
-}
-.card {
-    box-shadow: 0 0.15rem 1.75rem 0 rgb(33 40 50 / 15%);
-}
-.card .card-header {
-    font-weight: 500;
-}
-.card-header:first-child {
-    border-radius: 0.35rem 0.35rem 0 0;
-}
-.card-header {
-    padding: 1rem 1.35rem;
-    margin-bottom: 0;
-    background-color: rgba(33, 40, 50, 0.03);
-    border-bottom: 1px solid rgba(33, 40, 50, 0.125);
-}
-.form-control, .dataTable-input {
-    display: block;
-    width: 100%;
-    padding: 0.875rem 1.125rem;
-    font-size: 0.875rem;
-    font-weight: 400;
-    line-height: 1;
-    color: #69707a;
-    background-color: #fff;
-    background-clip: padding-box;
-    border: 1px solid #c5ccd6;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-    border-radius: 0.35rem;
-    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-}
-
-.nav-borders .nav-link.active {
-    color: #0061f2;
-    border-bottom-color: #0061f2;
-}
-.nav-borders .nav-link {
-    color: #69707a;
-    border-bottom-width: 0.125rem;
-    border-bottom-style: solid;
-    border-bottom-color: transparent;
-    padding-top: 0.5rem;
-    padding-bottom: 0.5rem;
-    padding-left: 0;
-    padding-right: 0;
-    margin-left: 1rem;
-    margin-right: 1rem;
-}
-
-.btn-danger-soft {
-    color: #000;
-    background-color: #f1e0e3;
-    border-color: #f1e0e3;
-}
-    </script>
+   
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+        <nav class="navbar navbar-expand-md navbar-light text-white  shadow-sm"  style="background: #fa8f3d">
+            <div class="container" >
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Project') }}
                 </a>
@@ -97,9 +31,9 @@ color:#69707a;
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse"  id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                    <ul class="navbar-nav mx-auto">
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('user.home') }}">Home</a>
                         </li>
@@ -109,7 +43,7 @@ color:#69707a;
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav mx-auto">
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
